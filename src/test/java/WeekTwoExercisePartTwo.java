ab26d83ddcf9ccfcebfded2395b4ef4c01e51881
RobertB <jennbobblomberg@gmail.com>
2021-02-23 14:21:01 -0600

    fizzbuzz multiple of 3 and 5

--- src/test/java/WeekTwoExercisePartTwo.java
+++ src/test/java/WeekTwoExercisePartTwo.java
@@ -90,7 +90,6 @@ public class WeekTwoExercisePartTwo {
     }
 
     @Test
-    @Disabled
     public void sendingAnyMultipleOfThreeAndFiveOfThreeShouldReturnStringFizzBuzz() {
         // TODO Comment: replace this comment with a brief description of why you made the change you did in FizzBuzz.java
         // TODO: Implement the code to make this test pass
