315d6dc6c260a430024188df03ca25ab80cb1636
RobertB <jennbobblomberg@gmail.com>
2021-02-23 13:36:00 -0600

    fizzbuzz 3

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,9 @@
 
 public class FizzBuzz {
     public String execute(Integer number) {
-
+        if(number == 3){
+            return "Fizz";
+        }
         return number.toString();
     }
 }
